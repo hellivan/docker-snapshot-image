8d01bc335bd5cb36b5ede1a4fdf422b79c5ec590
Ivan Hell <hell.ivan1991@gmail.com>
2017-01-21 11:34:01 +0100

    fix: provide more meaningul documentation for -f flag

--- bin/docker-snapshot-image.js
+++ bin/docker-snapshot-image.js
@@ -6,7 +6,7 @@ const {createImage} = require('../lib');
 
 program.version(pkg.version)
     .option('--no-auto', 'Do not create the image with the automatic snapshot-tag ({pkg-version}-{commit-hash})')
-    .option('-f --fixed-tag <name>', 'Tag the image with the specified tag')
+    .option('-f --fixed-tag <name>', 'Additionally tag the image with the specified tag')
     .option('--image-name <name>', 'Use the specified custom name for the image')
     .option('-t --test', 'Start application in test mode (only log docker commands on stdout)')
     .option('--silent-docker', 'Do not output stdout/stderr from executed docker commands')
