ddafc24161a5d0f3b5e322e30d4d67eb519b42ad
Ivan Hell <hell.ivan1991@gmail.com>
2022-01-27 09:01:00 +0100

    chore: do not compile spec files on lib builds

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     displayName: 'unit',
     rootDir: '../../',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // TODO: remove (and remove from package) as soon as jest-circus is set to default runner
-    testRunner: 'jest-circus/runner',
     testMatch: ['**/src/**/*.spec.ts'],
+    ...require('../../ts-jest.config'),
 };
